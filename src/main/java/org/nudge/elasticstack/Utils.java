eb515b2c8f5b5fc43d673f0d579e387cb7fab282
thomas.arnaud <thomas.arnaud@gmail.com>
2017-06-22 19:21:08 +0200

    avoid useless DateFormat creation

--- src/main/java/org/nudge/elasticstack/Utils.java
+++ src/main/java/org/nudge/elasticstack/Utils.java
@@ -12,20 +12,28 @@ public class Utils {
 	private static final String ES_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
 	private static final String INDEX_SUFFIX_FMT = "yyyy-MM-dd";
 
-	private static final ThreadLocal<DateFormat> df = new ThreadLocal<>();
+	private static final ThreadLocal<DateFormat> indexf = new ThreadLocal<>();
+	private static final ThreadLocal<DateFormat> timef = new ThreadLocal<>();
 
 	private Utils() {
 	}
 
 	public static String formatTimeToString(long timestamp) {
-		df.set(new SimpleDateFormat(ES_DATE_FORMAT));
-		return df.get().format(timestamp);
+		DateFormat sdf = timef.get();
+		if (sdf == null) {
+			sdf = new SimpleDateFormat(ES_DATE_FORMAT);
+			timef.set(sdf);
+		}
+		return sdf.format(timestamp);
 	}
 
 	public static String getIndexSuffix() {
-		df.set(new SimpleDateFormat(INDEX_SUFFIX_FMT));
-		return df.get().format(new Date());
+		DateFormat sdf = indexf.get();
+		if (sdf == null) {
+			sdf = new SimpleDateFormat(INDEX_SUFFIX_FMT);
+			indexf.set(sdf);
+		}
+		return sdf.format(new Date());
 	}
-
 
 }
